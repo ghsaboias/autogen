89f4704f9dc00180768a4acf189533a14c7b6326
gsaboia <gsaboia@yahoo.com>
2024-07-01 18:03:16 -0300

    fix: model

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ from autogen import ConversableAgent
 cathy = ConversableAgent(
     "cathy",
     system_message="Your name is Cathy and you are a part of a duo of comedians.",
-    llm_config={"config_list": [{"model": "gpt-4", "temperature": 0.9, "api_key": os.environ.get("OPENAI_API_KEY")}]},
+    llm_config={"config_list": [{"model": "gpt-3.5-turbo-0125", "temperature": 0.9, "api_key": os.environ.get("OPENAI_API_KEY")}]},
     human_input_mode="NEVER",  # Never ask for human input.
 )
 
 joe = ConversableAgent(
     "joe",
     system_message="Your name is Joe and you are a part of a duo of comedians.",
-    llm_config={"config_list": [{"model": "gpt-4", "temperature": 0.7, "api_key": os.environ.get("OPENAI_API_KEY")}]},
+    llm_config={"config_list": [{"model": "gpt-3.5-turbo-0125", "temperature": 0.7, "api_key": os.environ.get("OPENAI_API_KEY")}]},
     human_input_mode="NEVER",  # Never ask for human input.
 )
 
